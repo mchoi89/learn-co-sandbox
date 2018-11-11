2f8ec974cfff02f4f6bf562d9581b77a0c25401f
mchoi89 <mchoi89@gmail.com>
2018-11-11 19:55:53 +0000

    automatically backed up by learn

---
+++ conditionals.rb
@@ -0,0 +1,30 @@
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def group_badge_maker(speakers)
+  speakers.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_conferenceroom(speakers)
+  room = 0
+  speakers.each do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to conference room #{conferenceroom}!"
+  end
+end
+
+
+def printer(speakers)
+  group_badge_maker(speakers).each do |result|
+    puts result
+  end
+
+  assign_conferenceroom(speakers).each do |result|
+    puts result
+  end
+end
+end
+end
